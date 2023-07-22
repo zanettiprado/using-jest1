a21a9badd2e3d5e490633044635a1f2ec62bf250
zanettiprado <zanettiprado@gmail.com>
2023-07-22 15:52:31 +0000

    build advance tests interacting with DOM

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -5,7 +5,12 @@
 const buttonClick = require("../button");
 
 beforeEach (() => {
-    document.body.innerHTML = "<p id='par'></p>";
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+    
 });
 
 describe("DOM test", () => {
@@ -13,4 +18,7 @@ describe("DOM test", () => {
         buttonClick();
         expect(document.getElementById("par").innerHTML).toEqual("You Clicked");
     });
+    test("h1 should exist", () => {
+        expect(document.getElementsByTagName("h1").length).toBe(1); 
+    })
 })
